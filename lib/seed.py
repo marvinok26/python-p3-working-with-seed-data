6a0051d386237f3534a31cc4b41769e546c212c7
Marvin <okongomarvin971@gmail.com>
2024-03-31 23:52:53 +0300

    Completed Assignment

--- lib/seed.py
+++ lib/seed.py
@@ -4,13 +4,29 @@ from faker import Faker
 import random
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy import func
 
 from models import Game
 
 fake = Faker()
 
 if __name__ == '__main__':
-    
     engine = create_engine('sqlite:///seed_db.db')
     Session = sessionmaker(bind=engine)
     session = Session()
+
+    # Add a console message so we can see output when the seed file runs
+    print("Seeding games...")
+
+    games = [
+        Game(
+            title=fake.name(),
+            genre=fake.word(),
+            platform=fake.word(),
+            price=random.randint(0, 60)
+        )
+        for i in range(50)
+    ]
+
+    session.bulk_save_objects(games)
+    session.commit()
